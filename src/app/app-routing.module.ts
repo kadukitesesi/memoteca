f3f18cbf6ab03c28ff996635433cd4d732f2d555
kadukitesesi <kadukitesesi@gmail.com>
2025-12-28 19:14:38 -0300

    feat: configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionFormComponent } from './components/section-form/section-form.component';
+import { SectionBulletinComponent } from './components/section-bulletin/section-bulletin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo:'section-bulletin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'section-form',
+    component: SectionFormComponent
+  },
+  {
+    path: 'section-bulletin',
+    component: SectionBulletinComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
